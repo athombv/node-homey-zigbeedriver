7e0e51c3b0cd8644792211924d7ed765994058bb
Robin Bolscher <robinbolscher@gmail.com>
2020-03-17 11:21:39 +0100

    Improve grunt setup by adding connect
    This implements live reload in combination with `grunt watch`.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,28 @@
 'use strict';
 
+const SERVE_DOCS_PORT = 34567;
+
 module.exports = grunt => {
   grunt.initConfig({
     jsdoc: {
       dist: {
         src: ['lib/**/*.js'],
         options: {
-          configure: 'jsdoc.json', destination: 'docs',
+          configure: 'jsdoc.json',
+          destination: 'docs',
+        },
+      },
+    },
+    connect: {
+      server: {
+        options: {
+          port: SERVE_DOCS_PORT,
+          base: 'docs',
+          keepalive: true, // Keep the server alive indefinitely
+          livereload: true, // Enable live reload (i.c.w. grunt watch)
+          open: {
+            target: `http://localhost:${SERVE_DOCS_PORT}`, // Open docs in browser automatically
+          },
         },
       },
     },
@@ -15,11 +31,13 @@ module.exports = grunt => {
         files: ['lib/**/*.js'],
         tasks: ['jsdoc'],
         options: {
-          spawn: false,
+          spawn: true,
+          livereload: true, // Enable live reload (i.c.w. grunt connect)
         },
       },
     },
   });
   grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
 };
