865d1c18b39011a9451d7aedbf6bb3f02656833e
Robin Bolscher <robinbolscher@gmail.com>
2020-03-24 11:31:43 +0100

    Remove non-docs files

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports.Util = require('./lib/util');
-
-module.exports.ZigBeeDevice = require('./lib/ZigBeeDevice.js');
-module.exports.ZigBeeLightDevice = require('./lib/ZigBeeLightDevice.js');
-module.exports.ZigBeeXYLightDevice = require('./lib/ZigBeeXYLightDevice.js');
