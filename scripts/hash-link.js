fee2a0f773c74be25148a71aeec3d529bca08bbe
RobinBol <RobinBol@users.noreply.github.com>
2021-06-10 12:17:52 +0000

    deploy: 258b93643fe8ccab17ea7007d5788b038e544399

---
+++ scripts/hash-link.js
@@ -0,0 +1,34 @@
+(function () {
+  "use strict"
+
+  documentReady(function () {
+    const $hashLinks = document.querySelectorAll(`[data-hash-link]`);
+
+    Array.from($hashLinks).forEach(($hashLink) => {
+      $hashLink.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        navigator.clipboard.writeText($hashLink.href);
+
+        const id = $hashLink.href.split('#')[1];
+        document.getElementById(id).scrollIntoView({
+          behavior: "smooth"
+        });
+
+        const toastMessage = document.createElement('div');
+        toastMessage.classList.add('toastmessage');
+        toastMessage.textContent += 'Link copied to clipboard:';
+
+        const toastLink = document.createElement('div');
+        toastLink.classList.add('toastmessage__link');
+        toastLink.textContent += $hashLink.href;
+        toastMessage.appendChild(toastLink);
+        document.body.appendChild(toastMessage);
+
+        setTimeout(function () {
+          document.body.removeChild(toastMessage)
+        }, 4000);
+      })
+    });
+  });
+})();
